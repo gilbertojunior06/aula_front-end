224c1d1d784408ab2674a8d7db40c5f2344660d1
Gilberto Junior <gilbertosilveriojr@gmail.com>
2026-01-29 22:30:33 -0300

    adicionando novos arquivos

--- script.js
+++ script.js
@@ -2,4 +2,6 @@ import open from "open";
 
 const url = "https://gilbertojunior06.github.io/aula_front-end/";
 
-open(url);
+open(url)
+  .then(() => console.log("🌐 Site aberto no navegador!"))
+  .catch(err => console.error("❌ Erro ao abrir site:", err));
