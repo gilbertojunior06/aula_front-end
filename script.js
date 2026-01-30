1a8e6e3db93124f88d15d392b44bd64904ce164f
Gilberto Junior <gilbertosilveriojr@gmail.com>
2026-01-29 22:34:46 -0300

    adicionando novos arquivos

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ import open from "open";
 
 const url = "https://gilbertojunior06.github.io/aula_front-end/";
 
-open(url)
-  .then(() => console.log("🌐 Site aberto no navegador!"))
+// Forçar abrir no Brave
+open(url, { app: { name: "brave" } })
+  .then(() => console.log("🌐 Site aberto no Brave!"))
   .catch(err => console.error("❌ Erro ao abrir site:", err));
